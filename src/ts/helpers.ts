906b0e2c06b3ae10bc3eba5b12f02aafb97139f5
luckmer <woszczo04@gmail.com>
2021-08-19 20:38:42 +0200

    dist / helpers

--- src/ts/helpers.ts
+++ src/ts/helpers.ts
@@ -4,7 +4,11 @@ export class helpers {
   filterFunc = (arr: HTMLDivElement[], mutate: mutation) =>
     arr.filter(mutate).map(({ id }) => Number(id));
 
-  endBlocks = (arr: HTMLDivElement[], mutate: mutation) => arr.filter(mutate);
+  endBlocks = (arr: HTMLDivElement[], mutate: number) =>
+    arr.slice(mutate).map(({ id }) => Number(id));
+
+  startBlocks = (arr: HTMLDivElement[], mutate: number) =>
+    arr.slice(0, mutate).map(({ id }) => Number(id));
 
   wallEnd = (arr: number[], mutate: mutatesWall) => arr.some(mutate);
 }
