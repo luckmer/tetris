1e3a4e7f4f3a59c836d63f125ac50a0b1c6f76ed
luckmer <woszczo04@gmail.com>
2021-08-19 20:34:13 +0200

    global data

---
+++ src/ts/GlobalData.ts
@@ -0,0 +1,22 @@
+export class GlobalData {
+  public static down: number = 3;
+  public static rotates: number = 0;
+  public static gameOver: boolean = false;
+  public static score: number = 0;
+  public static lines: number = 0;
+  public static level: number = 0;
+  public static LINE_LEVEL_INCREMENT: number = 3;
+
+  public static backgrounds: string[] = [
+    "REBECCAPURPLE",
+    "BLUEVIOLET",
+    "DARKVIOLET",
+    "DARKORCHID",
+    "DARKMAGENTA",
+    "PURPLE",
+    "INDIGO",
+    "SLATEBLUE",
+    "DARKSLATEBLUE",
+    "MEDIUMSLATEBLUE",
+  ];
+}
