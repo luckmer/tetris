6cbd655d28cf04c09384a3f3633fd7c9b18c4f73
luckmer <woszczo04@gmail.com>
2021-08-21 20:22:07 +0200

    incorrect code

--- src/ts/interfaces.ts
+++
@@ -1,9 +0,0 @@
-export type mutation = (v: HTMLDivElement) => boolean;
-export type mutatesWall = (v: number) => boolean;
-
-export interface piecesMap {
-  [key: string]: {
-    matrix: number[][];
-    color: string;
-  };
-}
