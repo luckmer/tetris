33b43bda34c39f68d2a820a4d725c5a6a7a861a9
luckmer <woszczo04@gmail.com>
2021-08-21 20:19:22 +0200

    main constants folder

---
+++ src/ts/constants/GlobalData.ts
@@ -0,0 +1,19 @@
+export class GlobalData {
+  public static down: number = 3;
+
+  public static rotates: number = 0;
+
+  public static gameOver: boolean = false;
+
+  public static score: number = 0;
+
+  public static lines: number = 0;
+
+  public static level: number = 0;
+
+  public static LINE_LEVEL_INCREMENT: number = 3;
+
+  public static blockMove: boolean = false;
+
+  public static stop: boolean = false;
+}
