eb55a72bf55bdb1b0f671d59ff4aefab8ca91698
luckmer <woszczo04@gmail.com>
2021-08-21 20:03:52 +0200

    added mouse click

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,12 @@
-import { Joystick } from "./Joystick";
+import { Joystick } from "./joystick/Joystick";
+import { buttons } from "./constants/html";
 
 export const joystick = new Joystick();
+
+buttons.forEach((button) =>
+  button.addEventListener("click", joystick.mouseControl)
+);
+
 document?.addEventListener("keydown", joystick.control);
+
+// add status about game pause
